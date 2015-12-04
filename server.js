3726f81ac3a9821f4d4aeb882315485a7c6c4cf0
Jack Connor <jack.connor83@gmail.com>
2015-12-04 10:57:41 -0800

    kdsjfsld

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express        = require('express');
 var path           = require('path');
 var dotenv         = require('dotenv').load();
+console.log(process.env);
 // var favicon = require('serve-favicon');
 var logger         = require('morgan');
 var cookieParser   = require('cookie-parser');
