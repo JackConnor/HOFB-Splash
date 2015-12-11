49102d50f8f91837a56a78114e371c9d5705c019
Jack Connor <jack.connor83@gmail.com>
2015-12-11 13:57:54 -0800

    multer taken out to attempt heroku fix

--- api.js
+++ api.js
@@ -10,7 +10,7 @@ var route          = express.Router();
 var bcrypt         = require('bcrypt-nodejs');
 var passport       = require('passport');
 var passportLocal  = require('passport-local');
-var multer         = require('multer');
+// var multer         = require('multer');
 // app.use(bodyParser.json());
 
 //
@@ -273,45 +273,45 @@ module.exports = function(app){
 
   ///////////////////////////////////////
   /////Begin photo uploading logic///////
-  var uploading = multer({
-    dest: __dirname + '../public/uploads/',
-  })
-
-  app.post('/api/pictures/upload', multer({ dest: './uploads/'}).single('upl'), function(req,res){
-  	// console.log(req.body); //form fields
-  	/* example output:
-  	{ title: 'abc' }
-  	 */
-
-   console.log('yoyo');
-   console.log(req.file); //form files
-   req.file.fieldname = req.file.originalname
-   req.body.fieldname = req.body.originalname
-  //  var thisPhoto = require("./uploads/"+req.body.originalname)
-  //  console.log(thisPhoto);
-  //  req.file.encoding = '64bit';
-  //  Photo.create({
-  //    photoData: thisPhoto
-  //    ,author: "jack connor"
-  //    ,title: "first test photo, baby"
-  //  }, function(err, newPhoto){
-  //    console.log(newPhoto);
-  //  })
-   console.log(req.file);
-   console.log(req.body);
-   console.log('yoyo');
-  	/* example output:
-              { fieldname: 'upl',
-                originalname: 'grumpy.png',
-                encoding: '7bit',
-                mimetype: 'image/png',
-                destination: './uploads/',
-                filename: '436ec561793aa4dc475a88e84776b1b9',
-                path: 'uploads/436ec561793aa4dc475a88e84776b1b9',
-                size: 277056 }
-  	 */
-  	res.status(204).end();
-  });
+  // var uploading = multer({
+  //   dest: __dirname + '../public/uploads/',
+  // })
+  //
+  // app.post('/api/pictures/upload', multer({ dest: './uploads/'}).single('upl'), function(req,res){
+  // 	// console.log(req.body); //form fields
+  // 	/* example output:
+  // 	{ title: 'abc' }
+  // 	 */
+  //
+  //  console.log('yoyo');
+  //  console.log(req.file); //form files
+  //  req.file.fieldname = req.file.originalname
+  //  req.body.fieldname = req.body.originalname
+  // //  var thisPhoto = require("./uploads/"+req.body.originalname)
+  // //  console.log(thisPhoto);
+  // //  req.file.encoding = '64bit';
+  // //  Photo.create({
+  // //    photoData: thisPhoto
+  // //    ,author: "jack connor"
+  // //    ,title: "first test photo, baby"
+  // //  }, function(err, newPhoto){
+  // //    console.log(newPhoto);
+  // //  })
+  //  console.log(req.file);
+  //  console.log(req.body);
+  //  console.log('yoyo');
+  // 	/* example output:
+  //             { fieldname: 'upl',
+  //               originalname: 'grumpy.png',
+  //               encoding: '7bit',
+  //               mimetype: 'image/png',
+  //               destination: './uploads/',
+  //               filename: '436ec561793aa4dc475a88e84776b1b9',
+  //               path: 'uploads/436ec561793aa4dc475a88e84776b1b9',
+  //               size: 277056 }
+  // 	 */
+  // 	res.status(204).end();
+  // });
 
 
 // var upload = multer({ storage: storage })
