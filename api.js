908973afdfe38d629d472b8622c49809952041ea
Jack Connor <jack.connor83@gmail.com>
2015-12-10 20:30:06 -0800

    file upload to our site works

--- api.js
+++ api.js
@@ -10,6 +10,10 @@ var route          = express.Router();
 var bcrypt         = require('bcrypt-nodejs');
 var passport       = require('passport');
 var passportLocal  = require('passport-local');
+var multer         = require('multer');
+// app.use(bodyParser.json());
+
+//
 
 require('./passport.js')(passport);
 
@@ -264,6 +268,35 @@ module.exports = function(app){
 
   ///////End Signup, Login, Authorization, and Sessions
   ///////////////////////////////////////////////////////
+
+  ///////////////////////////////////////
+  /////Begin photo uploading logic///////
+  var uploading = multer({
+    dest: __dirname + '../public/uploads/',
+  })
+
+  app.post('/pictures/upload', multer({ dest: './uploads/'}).single('upl'), function(req,res){
+  	console.log(req.body); //form fields
+  	/* example output:
+  	{ title: 'abc' }
+  	 */
+  	console.log(req.file); //form files
+  	/* example output:
+              { fieldname: 'upl',
+                originalname: 'grumpy.png',
+                encoding: '7bit',
+                mimetype: 'image/png',
+                destination: './uploads/',
+                filename: '436ec561793aa4dc475a88e84776b1b9',
+                path: 'uploads/436ec561793aa4dc475a88e84776b1b9',
+                size: 277056 }
+  	 */
+  	res.status(204).end();
+  });
+
+  /////End photo uploading logic/////////
+  ///////////////////////////////////////
+
 
   //////////////////////////////////////////
   /////begin email stuff////////////////////
