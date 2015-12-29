f673d9ab1dd67d5adb49e1572031d92725a220e1
Jack Connor <jack.connor83@gmail.com>
2015-12-28 20:10:15 -0800

    first version of messages done

--- public/js/controllers/messageController.js
+++ public/js/controllers/messageController.js
@@ -45,19 +45,6 @@ angular.module('messageController', ['allMessagesFactory', 'checkPwFactory'])
     }
 
     function addInteractionToMessages(convoList){
-      $('.messageListContentTitle').on('mouseenter', function(evt){
-        // var elemen = $(evt.target)[0].parentNode
-        // console.log(elemen);
-        // elemen.css({
-        //   backgroundColor: "#B2B2B4"
-        //   ,color: 'black'
-        // })
-      })
-      $('.messageListContentTitle').on('mouseleave', function(evt){
-        // var elemen = $($($(evt.target)[0].parentNode)[0].parentNode);
-        // console.log($(elemen));
-        // console.log(elemen.attr('id'));
-      })
       $('.messageContentHolder').on('mouseleave', function(evt){
         for (var i = 0; i < $('.messagesCell').length; i++) {
           $($('.messagesCell')[i]).css({
@@ -66,7 +53,6 @@ angular.module('messageController', ['allMessagesFactory', 'checkPwFactory'])
           })
         }
         for (var i = 0; i < $('.messageListContentTitle').length; i++) {
-          console.log($($('.messagesListContentTitle')[i]).css('background-color'));
           $($('.messageListContentTitle')[i]).css({
             'backgroundColor': "#B2B2B4"
             ,color: "black"
@@ -80,7 +66,6 @@ angular.module('messageController', ['allMessagesFactory', 'checkPwFactory'])
         }
       })
       $('.messageContentHolder').on('mouseenter', function(evt){
-        console.log($(evt.target));
         var elemen = $($(evt.target)[0].parentNode);
         elemen.css({
           backgroundColor: "black"
@@ -102,38 +87,32 @@ angular.module('messageController', ['allMessagesFactory', 'checkPwFactory'])
         window.location.hash = "#/view/product/"+productId;
       })
       $('.messageContentHolder').on('click', function(evt){
-        console.log(evt.target.id);
+        $('.messageChatWindowList').html('');
         for (var i = 0; i < convoList.length; i++) {
           if(convoList[i]._id == evt.target.id){
+            console.log(convoList[i].comments.length);
             for (var j = 0; j < convoList[i].comments.length; j++) {
-              console.log(convoList[i].comments[j].messageText);
-              $('.messagesSingleContainer').html('')
-              $('.messagesSingleContainer').append(
-                '<div class="messageContent">'+
-                  "<p class='messageSender'>"+convoList[i].comments[j].sender+"</p>"+
-                  "<p class='messageText'>"+convoList[i].comments[j].messageText+"</p>"+
-                "</div>"
-              );
+              if(j%2 == 0){
+                $('.messageChatWindowList').append(
+                  '<div class="messageContent">'+
+                    "<p class='messageSender'>"+convoList[i].comments[j].sender+"</p>"+
+                    "<p class='messageText'>"+convoList[i].comments[j].messageText+"</p>"+
+                  "</div>"
+                )
+              }
+              else {
+                $('.messageChatWindowList').append(
+                  '<div class="messageContentOdd">'+
+                    "<p class='messageTextOdd'>"+convoList[i].comments[j].messageText+"</p>"+
+                  "</div>"
+                )
+              }
             }
           }
         }
       })
     }
     allMessagesFunc(addEmailHtml);/////call the function to load all messages
-
-    ///////only for single message page, which is no longer in our final design
-    // if(window.location.hash.split('/')[1] == 'message'){
-    //   var messageId = window.location.hash.split('/')[2];
-    //   $http({
-    //     method: "GET"
-    //     ,url: "/api/comment/"+window.location.hash.split("/")[2]
-    //     // +window.location.hash.split('/')[2]
-    //   })
-    //   .then(function(comment){
-    //     self.sender = comment.data.sender;
-    //     self.commentText = comment.data.commentText;
-    //   })
-    // }
 
     ///logout button functionality
     $('.logoutButton').on('click', function(){
