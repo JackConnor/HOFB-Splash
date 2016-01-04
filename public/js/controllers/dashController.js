c6e225e103f717f3dee0174835e82f672fc283ea
Jack Connor <jack.connor83@gmail.com>
2016-01-03 21:53:42 -0800

    remerging frontend changes

--- public/js/controllers/dashController.js
+++ public/js/controllers/dashController.js
@@ -76,8 +76,9 @@ angular.module('dashController', ['allProjectsFactory', 'checkPwFactory', 'getSw
                   if(projMonth == nowMonth){
                     return "11 months old"
                   }
-                  else { 
+                  else {
                     var mSince = ((12+nowMonth) - projMonth);
+                    
                     if(mSince == 1){
                       return  "less than "+(mSince+" month old");
                     }
