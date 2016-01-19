50af853fc0f2c6d92d65a762a16745f01ac4f3c9
Jack Connor <jack.connor83@gmail.com>
2016-01-19 11:12:16 -0800

    reset api photo crop sizing

--- api.js
+++ api.js
@@ -565,7 +565,7 @@ module.exports = function(app){
         eager: [
            { width: 150, height: 150,
              crop: "fill", format: "png" },
-           { width: 500, height: 500,
+           { width: 560, height: 700,
             crop: 'fill', format: "png" }
         ]
        })
